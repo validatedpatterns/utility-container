50d9c7495861492a8584c63a2810249aaf3df5c0
Michele Baldessari <michele@acksyn.org>
2026-02-05 18:31:20 +0100

    Add wrapper for ansible-playbook
    This is needed because with ansible-2.19 our printf >/dev/tty hack
    is no longer working, so we need to switch to our own output plugin.
    
    The problem is that the "null" output plugin has been hardcoded in
    the patternizer so what we do is use our own plugin if we see it is
    set the null.

---
+++ ansible-playbook-wrapper.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Wrapper script for ansible-playbook that sets ANSIBLE_STDOUT_CALLBACK when needed
+if [ "${ANSIBLE_STDOUT_CALLBACK}" = "null" ]; then
+    export ANSIBLE_STDOUT_CALLBACK="rhvp.cluster_utils.readable"
+fi
+exec /usr/local/bin/ansible-playbook.orig "$@"
